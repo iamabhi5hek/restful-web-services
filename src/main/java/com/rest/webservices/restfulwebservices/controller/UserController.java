c390746a2422e88bf0e3df4a0472519e59715f4e
Abhishek <choudharyabhishek162@gmail.com>
2022-08-15 13:24:11 +0530

    Handling exception

--- src/main/java/com/rest/webservices/restfulwebservices/controller/UserController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.rest.webservices.restfulwebservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rest.webservices.restfulwebservices.dao.UserServiceDAOImpl;
+import com.rest.webservices.restfulwebservices.exception.UserNotFoundException;
 import com.rest.webservices.restfulwebservices.factory.UserFactory;
 
 @RestController
@@ -17,14 +23,38 @@ public class UserController {
 	private UserServiceDAOImpl userServiceDAOImpl;
 
 	//retrieve all users
-	@GetMapping(path = "/users")
+	@GetMapping("/users")
 	public List<UserFactory> retrieveAllUsers(){
 		return userServiceDAOImpl.findAll();
 	}
 	
 	//retrieve user(by id)
-	@GetMapping(path = "/users/{id}")
+	@GetMapping("/users/{id}")
 	public UserFactory retrieveById(@PathVariable int id) {
-		return userServiceDAOImpl.findOne(id);
+		
+		UserFactory user = userServiceDAOImpl.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		return user;
 	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody UserFactory user) {
+		UserFactory savedUser = userServiceDAOImpl.save(user);
+		
+		//now we need to pass the status of request
+		//we use ResponseEntity
+		 
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();		
+	}
+	
 }
