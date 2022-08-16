79464c33e89d844403974a307c80505c9ce20d7b
Abhishek <choudharyabhishek162@gmail.com>
2022-08-16 22:56:47 +0530

    Adding swagger dependency

--- src/main/java/com/rest/webservices/restfulwebservices/controller/HelloWorldContoller.java
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/HelloWorldContoller.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
